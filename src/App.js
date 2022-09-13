a9ad62a866720dc1bd509e3209d287fa378edb08
Ashe546 <asfyd42@gmail.com>
2022-09-13 04:48:23 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from "react";
+import todo from "./todo";
+import TodoList from "./TodoList";
+
+
+const LOCAL_STORAGE_KEY = 'todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef =useRef()
+
+  useEffect(()=> {
+    const storedTodo = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+   if (storedTodo) setTodos(storedTodo)
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handelAddTodo(e) {
+       const name = todoNameRef.current.value
+       if (name === '') return
+       setTodos(prevTodos => {
+        return [...prevTodos, {id: 2, name: name, completed: false }]
+       })
+        todoNameRef.current.value = null
+       
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <TodoList  todos={todos}/>
+      <input ref={todoNameRef} type = "text" />
+      <button onClick={handelAddTodo}>Add TodoList</button>
+      <button>clear complet TodoList</button>
+      <div>{todos.length} left todo</div>
+    </>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './todo'
+
+
+export default function TodoList({todos}) {
+  return (
+  todos.map(todo  => {
+    return <Todo key={todo.id} todo={todo} />
+  })
+  )
+}
